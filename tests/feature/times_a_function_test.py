340f73a9216c52b4b2fc207589b7b7273a143851
Stephen Kahn <steve@kahn.org.uk>
2019-05-01 15:56:57 +0100

    Added csv file reading to test

--- tests/feature/times_a_function_test.py
+++ tests/feature/times_a_function_test.py
@@ -1,6 +1,7 @@
 import pytest
 import sys
 import os
+import csv
 
 sys.path.insert(0,os.path.abspath("lib"))
 
@@ -9,5 +10,17 @@ import timer
 class TestClass():
     def test_passes_a_result_to_a_csv_file(self):
         stopwatch = timer.Timer()
+        def add(a,b):
+            return a + b
         function = lambda a,b :  a + b
-        stopwatch.start(function(1,1))
+        stopwatch.start(add(1,1))
+        # open csv file
+        with open('add.csv', 'r') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in spamreader:
+                    print(row)
+                    assert len(row) > 0
+                    return
+            assert True == False 
+                            
+        # assert contents 
